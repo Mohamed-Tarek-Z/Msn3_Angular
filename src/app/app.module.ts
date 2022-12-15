0e71db0393e31dee3a643752332cde70bd7e9dc7
Mohamed-Tarek-Z <MohamedTarek153@gmail.com>
2022-12-15 21:15:47 +0200

    last pc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HttpClientModule } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +25,7 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -55,6 +55,7 @@ import { FormsModule } from '@angular/forms';
     MatSelectModule,
     MatProgressSpinnerModule,
     MatTabsModule,
+    MatGridListModule,
 
     FormsModule,
     AppRoutingModule
